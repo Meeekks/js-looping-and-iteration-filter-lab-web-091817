686dbe279fa2e408f1274aaaa6726a1a0a8dc24f
github username <github email address>
2017-10-29 22:46:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  return array.filter(function(element) {
+    return element.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(function(element){
+    const eleSliced = element.slice(0, string.length)
+    if (eleSliced === string) {
+      return element;
+    }
+  });
+}
+
+function matchName(array, string) {
+  return array.filter(function(element) {
+    return element['name'] === string;
+  });
+}
